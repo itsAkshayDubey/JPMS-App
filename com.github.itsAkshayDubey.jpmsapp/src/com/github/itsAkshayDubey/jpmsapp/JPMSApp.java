b43d27ccfefb25a26cf2c7ff964a8392746ef327
Akshay Dubey <akshay.dubey055@gmail.com>
2022-08-09 11:36:26 +0530

    Add main method

--- com.github.itsAkshayDubey.jpmsapp/src/com/github/itsAkshayDubey/jpmsapp/JPMSApp.java
+++ com.github.itsAkshayDubey.jpmsapp/src/com/github/itsAkshayDubey/jpmsapp/JPMSApp.java
@@ -1,10 +1,17 @@
 package com.github.itsAkshayDubey.jpmsapp;
 
+import com.github.itsAkshayDubey.jpmsserver.JPMSServer;
+import com.github.itsAkshayDubey.jpmsserver.serverone.JPMSServerOne;
+import com.github.itsAkshayDubey.jpmsserver.servertwo.JPMSServerTwo;
 
 public class JPMSApp {
 
     public static void main(String[] args) {
-     
+
+        JPMSServer serverOne = new JPMSServerOne();
+        JPMSServer serverTwo = new JPMSServerTwo();
+        serverOne.checkStatus();
+        serverTwo.checkStatus();
 
     }
 }
