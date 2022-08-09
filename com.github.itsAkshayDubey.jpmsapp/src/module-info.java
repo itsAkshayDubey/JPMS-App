ef30f65aeb0191fb68228d6ff21e3904037aea96
Akshay Dubey <akshay.dubey055@gmail.com>
2022-08-09 11:36:12 +0530

    Add dependencies to module-info,java

--- com.github.itsAkshayDubey.jpmsapp/src/module-info.java
+++ com.github.itsAkshayDubey.jpmsapp/src/module-info.java
@@ -1,4 +1,7 @@
 module com.github.itsAkshayDubey.jpmsapp {
+    requires com.github.itsAkshayDubey.jpmsserver;
+    requires com.github.itsAkshayDubey.jpmsserver.serverone;
+    requires com.github.itsAkshayDubey.jpmsserver.servertwo;
 
     exports com.github.itsAkshayDubey.jpmsapp;
 }
